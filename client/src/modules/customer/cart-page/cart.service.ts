21c7a900dacd513fefea4edc67660adf4cbbd200
miri <mirmanasreh@aau.at>
2026-01-26 02:44:12 +0100

    Fix CartService

---
+++ client/src/modules/customer/cart-page/cart.service.ts
@@ -0,0 +1,72 @@
+import { Injectable } from '@angular/core';
+
+export interface CartItem{
+    dishId: number;
+    name: string;
+    price: number;
+    quantity: number;
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CartService{
+    private readonly STORAGE_KEY = 'user_cart_items';
+
+    getCustomerCart(): CartItem[] {
+        const savedItems = localStorage.getItem(this.STORAGE_KEY);
+        return savedItems ? JSON.parse(savedItems): [];
+    }
+
+    saverCartToLocal(items: CartItem[]): void{
+        localStorage.setItem(this.STORAGE_KEY, JSON.stringify(items));
+    }
+
+    addToCart(item: CartItem): void{
+        const currentCart = this.getCustomerCart();
+        const index = currentCart.findIndex(cartItem => cartItem.dishId === item.dishId);
+        if(index !== -1) {
+            currentCart[index].quantity += item.quantity;
+        } else {
+            currentCart.push(item);
+        }
+
+        this.saverCartToLocal(currentCart);
+    }
+    removeFromCart(removeDish: number): void{
+        let currentCart = this.getCustomerCart();
+        currentCart = currentCart.filter(item => item.dishId !== removeDish);
+        this.saverCartToLocal(currentCart);
+    }
+
+    changeQuantity(dishId: number, newQ: number): void {
+        if(newQ < 1) {
+            this.removeFromCart(dishId);
+        } 
+
+        const currentCart = this.getCustomerCart();
+        const index = currentCart.findIndex(item => item.dishId === dishId);
+
+        if(index !== -1) {
+            currentCart[index].quantity = newQ;
+            this.saverCartToLocal(currentCart);
+        }
+    }
+
+    clearCart(): void{
+        localStorage.removeItem(this.STORAGE_KEY);
+    }
+
+    getTotal(): number{
+        const items = this.getCustomerCart();
+        return items.reduce((total,item)=> total + (item.price * item.quantity), 0);
+    }
+
+    async submitOrderToServer(restaurantId: number, items: CartItem[], discountCode?: string):
+    Promise<{ order_number: number }> {
+        
+        return Promise.resolve({
+            order_number: Math.floor(Math.random() * 1000)
+        });
+    
+}
