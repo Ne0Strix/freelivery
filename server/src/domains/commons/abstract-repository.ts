37bdf43ea064515eb72e1030d93193261b1d8c29
miri <mirmanasreh@aau.at>
2026-01-26 17:38:28 +0100

    fixed issues

--- server/src/domains/commons/abstract-repository.ts
+++ server/src/domains/commons/abstract-repository.ts
@@ -13,12 +13,18 @@ export abstract class Repository<T> {
         protected primaryKey: string
     ) {
         this.pool = new Pool({
-            user: process.env.POSTGRES_USER,
-            host: process.env.POSTGRES_HOST ?? 'db',
-            database: process.env.POSTGRES_DB,
-            password: process.env.POSTGRES_PASSWORD,
+            user:
+                process.env.POSTGRES_USER || process.env.DB_USER || 'postgres',
+            host:
+                process.env.POSTGRES_HOST || process.env.DB_HOST || 'localhost',
+            database:
+                process.env.POSTGRES_DB || process.env.DB_NAME || 'freelivery',
+            password:
+                process.env.POSTGRES_PASSWORD ||
+                process.env.DB_PASSWORD ||
+                'postgres',
             port: Number(
-                process.env.POSTGRES_PORT ?? process.env.DB_PORT ?? 5432
+                process.env.POSTGRES_PORT || process.env.DB_PORT || 5432
             ),
         });
 
