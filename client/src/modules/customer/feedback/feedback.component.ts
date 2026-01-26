f046376103bcee635705f090d323881d101c21e7
miri <mirmanasreh@aau.at>
2026-01-26 01:37:48 +0100

    Updated feedbacck component

--- client/src/modules/customer/feedback/feedback.component.ts
+++ client/src/modules/customer/feedback/feedback.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-feedback',
@@ -21,6 +22,8 @@ export class FeedbackComponent {
         'Very Good',
         'Excellent',
     ];
+
+    constructor(private router: Router) {}
 
     get ratingMessage(): string {
         return this.ratingValues[this.ratingSelected];
